bfbba10e51d369da482ab289ddc626bc6c2686af
Bùi Đình Hải <hai.bd.hp@gmail.com>
2022-08-16 23:10:15 +0700

    chân trời mới

--- app/src/main/java/com/ohno/mylauncher/TestBoosterFragment.kt
+++ app/src/main/java/com/ohno/mylauncher/TestBoosterFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_test_booster_layout.*
+import kotlinx.android.synthetic.main.test.*
 
 class TestBoosterFragment: Fragment() {
 
@@ -20,7 +22,14 @@ class TestBoosterFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        ripple.startRippleAnimation()
+//        ripple.startRippleAnimation()
+//        with(animation_view) {
+//            setMinFrame(30)
+//        }
+
+        animation_view.setOnClickListener {
+            Toast.makeText(requireContext(), "Clicked", Toast.LENGTH_SHORT).show()
+        }
     }
 
 }
